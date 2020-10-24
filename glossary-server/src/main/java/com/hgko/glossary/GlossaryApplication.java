e3de9f6bbdefb2f94c8d3c53562175328158e16c
hgko1207 <khkkhk1207@gmail.com>
2020-10-24 21:40:38 +0900

    웹 서버 설정 추가

--- glossary-server/src/main/java/com/hgko/glossary/GlossaryApplication.java
+++ glossary-server/src/main/java/com/hgko/glossary/GlossaryApplication.java
@@ -2,12 +2,18 @@ package com.hgko.glossary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GlossaryApplication {
+public class GlossaryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlossaryApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GlossaryApplication.class);
+	}
 }
--- glossary-server/src/main/java/com/hgko/glossary/controller/MainController.java
+++ glossary-server/src/main/java/com/hgko/glossary/controller/MainController.java
@@ -1,8 +1,18 @@
 package com.hgko.glossary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+	@GetMapping("/")
+    public String index(Model model) {
+		 return "redirect:home";
+    }
+	
+	@GetMapping("home")
+	public void home(Model model) {
+	}
 }
